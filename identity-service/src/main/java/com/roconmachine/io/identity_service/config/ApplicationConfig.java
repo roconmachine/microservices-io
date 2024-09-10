2b58fd5635d3b769297ca92f3c25f8da5ae54ba8
rezaulhasan <roconmachine@gmail.com>
2024-09-10 21:32:01 +0200

    create bean for notificationApi add tags in yaml open api file that needs to sync with notification service

--- identity-service/src/main/java/com/roconmachine/io/identity_service/config/ApplicationConfig.java
+++ identity-service/src/main/java/com/roconmachine/io/identity_service/config/ApplicationConfig.java
@@ -1,6 +1,9 @@
 package com.roconmachine.io.identity_service.config;
 
+import com.roconmachine.io.identity_service.noti.api.NotificationApi;
+import com.roconmachine.io.identity_service.noti.client.ApiClient;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +17,16 @@ public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+
+    @Value("$notification.url")
+    private String notificationUrl;
+    @Bean
+    public NotificationApi getNotificationApi(){
+        ApiClient client = new ApiClient();
+        client.setBasePath(notificationUrl);
+        return new NotificationApi(client);
     }
 
 
