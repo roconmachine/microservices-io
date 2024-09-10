b03a70fd374e80bd5b238abdd760ae891cb7c923
rezaulhasan <roconmachine@gmail.com>
2024-09-10 21:41:02 +0200

    version: 1.0.0

--- identity-service/src/main/java/com/roconmachine/io/identity_service/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/roconmachine/io/identity_service/IdentityServiceApplication.java
@@ -1,11 +1,13 @@
 package com.roconmachine.io.identity_service;
 
 
-import com.roconmachine.io.identity_service.noti.api.DefaultApi;
+
+import com.roconmachine.io.identity_service.noti.api.NotificationApi;
 import com.roconmachine.io.identity_service.noti.client.ApiClient;
 
 import com.roconmachine.io.identity_service.noti.models.MessageTemplate;
 import com.roconmachine.io.identity_service.noti.models.RequestNotification;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,11 +25,11 @@ public class IdentityServiceApplication implements CommandLineRunner {
 		SpringApplication.run(IdentityServiceApplication.class, args);
 	}
 
+	@Autowired
+	private NotificationApi notificationApi;
 	@Override
 	public void run(String... args) throws Exception {
-		ApiClient client = new ApiClient();
-		client.setBasePath("http://localhost:8071/v01/notification");
-		DefaultApi defaultApi = new DefaultApi(client);
+
 		RequestNotification requestNotification = new RequestNotification();
 		requestNotification.setType(RequestNotification.TypeEnum.EMAIL);
 		requestNotification.setHeader("this is the header");
@@ -35,6 +37,6 @@ public class IdentityServiceApplication implements CommandLineRunner {
 		requestNotification.setDestinations(Stream.of("roconmachine@gmail.com")
 				.collect(Collectors.toList()));
 		requestNotification.setTemplate(new MessageTemplate("name","template of the message"));
-		defaultApi.addNotification(requestNotification);
+		notificationApi.addNotification(requestNotification);
 	}
 }
