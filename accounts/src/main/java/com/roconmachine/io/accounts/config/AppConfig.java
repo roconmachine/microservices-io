b38275760e5baed39f5e86501d38945cc6c4733e
rezaulhasan <roconmachine@gmail.com>
2024-10-01 15:40:52 +0200

    dockerized done for config,accounts,discovery and gatekeeper account out of parent.io libs

--- accounts/src/main/java/com/roconmachine/io/accounts/config/AppConfig.java
+++ accounts/src/main/java/com/roconmachine/io/accounts/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.roconmachine.io.accounts.config;
 
-import com.roconmachine.io.parent.config.Config;
+
+
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
-public class AppConfig extends Config {
+public class AppConfig {
 }
