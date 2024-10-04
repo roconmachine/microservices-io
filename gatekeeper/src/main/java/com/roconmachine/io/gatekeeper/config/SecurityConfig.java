4f158a39562e28b6bb0b6067832efebde8b631f9
rezaulhasan <roconmachine@gmail.com>
2024-10-04 19:24:13 +0200

    api token varification successful and works !!!!

--- gatekeeper/src/main/java/com/roconmachine/io/gatekeeper/config/SecurityConfig.java
+++ gatekeeper/src/main/java/com/roconmachine/io/gatekeeper/config/SecurityConfig.java
@@ -8,7 +8,12 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,12 +21,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.
-                csrf(ServerHttpSecurity.CsrfSpec ::disable)
+        http
+                .cors(corsSpec -> corsSpec.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.addAllowedOrigin("http://localhost:4200");  // Allow specific origin
+                    config.addAllowedMethod("*");  // Allow all HTTP methods
+                    config.addAllowedHeader("*");  // Allow all headers
+                    config.setAllowCredentials(true);  // Allow credentials like cookies or authorization headers
+                    return config;
+                }))
+                .csrf(ServerHttpSecurity.CsrfSpec ::disable)
+
                 .authorizeExchange(auth -> auth.
-                        pathMatchers("/eureka/**")
-                        .permitAll()
+                        pathMatchers("/ping").permitAll()
                         .pathMatchers("/accounts/api/**").authenticated()
+
                         .anyExchange()
                         .authenticated()
                 )
@@ -49,5 +63,17 @@ public class SecurityConfig {
 
         return null; //new CorsWebFilter(source);
     }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(false);
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 }
 
---
+++ gatekeeper/src/main/java/com/roconmachine/io/gatekeeper/config/TraceWebFilter.java
@@ -0,0 +1,17 @@
+package com.roconmachine.io.gatekeeper.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class TraceWebFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        exchange.getRequest().mutate()
+                .header("traceId", "ANNOTATED-TRACE-ID");
+        return chain.filter(exchange);
+    }
+}
