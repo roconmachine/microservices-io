a2022d451fae06612688aeac6407efc218514f42
rezaulhasan <roconmachine@gmail.com>
2024-10-09 19:18:58 +0200

    docker and discovery server integration done

--- user-info/src/main/java/com/roconmachine/io/user_info/controller/IUserInformationReactive.java
+++ user-info/src/main/java/com/roconmachine/io/user_info/controller/IUserInformationReactive.java
@@ -3,13 +3,14 @@ package com.roconmachine.io.user_info.controller;
 import com.roconmachine.io.user_info.dto.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
+@RequestMapping("${service.name}/${service.apiversion}/reactive")
 public interface IUserInformationReactive {
-    @GetMapping("users")
+    @GetMapping("/users")
     public Flux<UserDetails> getAllUsers();
 
-    @GetMapping("user/{id}")
+    @GetMapping("/users/{id}")
     public Mono<UserDetails> getUserById(@PathVariable String id);
 }
--- user-info/src/main/java/com/roconmachine/io/user_info/service/KeycloakService.java
+++ user-info/src/main/java/com/roconmachine/io/user_info/service/KeycloakService.java
@@ -1,5 +1,6 @@
 package com.roconmachine.io.user_info.service;
 
+import com.roconmachine.io.user_info.config.KeycloakConfig;
 import com.roconmachine.io.user_info.dto.UserDetails;
 import org.keycloak.admin.client.Keycloak;
 import org.modelmapper.ModelMapper;
@@ -14,15 +15,17 @@ import java.util.stream.Collectors;
 public class KeycloakService implements IKeycloakService{
 
     private Keycloak keycloak;
+    private KeycloakConfig keycloakConfig;
     @Autowired
     private ModelMapper modelMapper;
-    public KeycloakService(Keycloak _Keycloak){
+    public KeycloakService(Keycloak _Keycloak, KeycloakConfig _KeycloakConfig){
         this.keycloak = _Keycloak;
+        this.keycloakConfig = _KeycloakConfig;
     }
 
     @Override
     public ResponseEntity<List<UserDetails>> getAllUsers() {
-        List<UserDetails> list = this.keycloak.realm("web-app").users().list().stream().map(
+        List<UserDetails> list = this.keycloak.realm(this.keycloakConfig.getRealm()).users().list().stream().map(
                 userRepresentation -> modelMapper.map(userRepresentation, UserDetails.class)
         ).collect(Collectors.toList());
         return ResponseEntity.ok(list);
@@ -30,7 +33,7 @@ public class KeycloakService implements IKeycloakService{
 
     @Override
     public ResponseEntity<UserDetails> getUserById(String userId) {
-        UserDetails userDetails = modelMapper.map(this.keycloak.realm("web-app").users().
+        UserDetails userDetails = modelMapper.map(this.keycloak.realm(this.keycloakConfig.getRealm()).users().
                         searchByUsername(userId, true).get(0),
                 UserDetails.class);
         return ResponseEntity.ok(userDetails);
