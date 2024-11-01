67d6bcfc042a8158c82379c064b19390df5b718f
rezaulhasan <roconmachine@gmail.com>
2024-11-01 11:13:44 +0100

    other files

---
+++ fgaccess/src/main/java/com/roconmachine/io/fgaccess/FgaccessApplication.java
@@ -0,0 +1,14 @@
+package com.roconmachine.io.fgaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FgaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgaccessApplication.class, args);
+	}
+
+
+
+}
---
+++ fgaccess/src/test/java/com/roconmachine/io/fgaccess/FgaccessApplicationTests.java
@@ -0,0 +1,18 @@
+package com.roconmachine.io.fgaccess;
+
+import com.roconmachine.io.fgaccess.service.PolicyMappingService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgaccessApplicationTests {
+
+    @Mock
+    private PolicyMappingService service;
+
+    @Test
+    public void dummy() {
+
+    }
+}
